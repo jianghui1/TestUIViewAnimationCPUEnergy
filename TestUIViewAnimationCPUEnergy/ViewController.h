767ccc7db4fc76645ba857f57e95543b45fbb5e2
jiangzenghui005@163.com <jiangzenghui005@163.com>
2019-03-11 15:41:45 +0800

    Initial Commit

---
+++ TestUIViewAnimationCPUEnergy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUIViewAnimationCPUEnergy
+//
+//  Created by ys on 2019/3/11.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestUIViewAnimationCPUEnergy/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestUIViewAnimationCPUEnergy
+//
+//  Created by ys on 2019/3/11.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
